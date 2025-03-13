7a3ad77ebf82a033c75a1a2136ba0c771685411c
Harsh04Gautam <harsh99gautam@gmail.com>
2025-03-13 15:10:44 +0000

    added some backend stuff for user

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,26 @@
+import catchAsync from "../utils/catchAsync.js";
+import User from "../modules/userModule.js";
+
+export const createUserHandler = catchAsync(async (req, res, _next) => {
+  req.body.email = req.body.email.toLowerCase();
+  const user = await User.create(req.body);
+
+  res.status(200).json({
+    status: "success",
+    message: "user created successfully",
+    id: user.id, //remove in prod
+  });
+});
+
+export const getCurrentUserHandler = catchAsync(async (_req, res, _next) => {
+  let user = await User.findById(res.locals.user._id)
+    .select("-password")
+    .lean();
+
+  return res.status(200).json({
+    status: "success",
+    data: {
+      user,
+    },
+  });
+});
