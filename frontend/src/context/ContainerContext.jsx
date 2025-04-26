2b3d6eae2050e24bc32eab4400d89fa9bcb61e86
tchiappa <tchiappa@stevens.edu>
2025-04-26 13:35:05 -0700

    Added tests for the context.

--- frontend/src/context/ContainerContext.jsx
+++ frontend/src/context/ContainerContext.jsx
@@ -1,10 +1,10 @@
-import {createContext, useContext, useState} from "react";
+import {createContext, useState} from "react";
 
 const ContainerContext = createContext({});
 
 export const ContainerProvider = ({ children }) => {
     const [sidebarsActive, setSidebarsActive] = useState(true);
-    const {tutorialActive, setTutorialActive} = useContext(ContainerContext);
+    const [tutorialActive, setTutorialActive] = useState(false);
     const [spaceStationActive, setSpaceStationActive] = useState(false);
 
     return (
