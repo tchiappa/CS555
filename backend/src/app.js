799261473eaad0819318a9557d6cf5ea0d7e49fc
Yedhupati Samanth mani veer <113627383+maniveer07@users.noreply.github.com>
2025-05-01 11:25:30 -0400

    Delete backend/src directory

--- backend/src/app.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const app = express();
-
-export default app;
--- backend/src/middlewares/userRequired.js
+++
@@ -1,9 +0,0 @@
-import AppError from "../utils/appError.js";
-
-export const userRequired = (_req, res, next) => {
-  const user = res?.locals?.user;
-  if (user) {
-    return next();
-  }
-  return next(new AppError("you are not logged in", 400));
-};
